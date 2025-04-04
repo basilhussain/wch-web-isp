23d256e45e6cc07fe6c26a63fe61edc2b362dff2
Basil Hussain <basilhussain@users.noreply.github.com>
2025-04-04 03:44:05 +0100

    Update release script to include docs and license in zip

--- release.bat
+++ release.bat
@@ -5,4 +5,4 @@ set OUTDIR=dist
 set RELDIR=release
 
 mkdir "%RELDIR%"
-"%ZIP%" a "%RELDIR%\wch-web-isp.zip" "%OUTDIR%\*"
+"%ZIP%" a "%RELDIR%\wch-web-isp.zip" "%OUTDIR%\*" "README.md" "LICENSE.txt"
