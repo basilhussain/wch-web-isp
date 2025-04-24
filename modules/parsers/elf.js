8ed28b2af74cbb61ec9397b49822d2870eabf832
Basil Hussain <basilhussain@users.noreply.github.com>
2025-04-24 14:26:39 +0100

    Add SPDX licence identifier to all source headers

--- modules/parsers/elf.js
+++ modules/parsers/elf.js
@@ -16,6 +16,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  * 
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * 
  ******************************************************************************/
 
 const ELF_MAGIC = 0x7F454C46; // 0x7F, 'E', 'L', 'F'
